8db2ecd5a3b9f9e0be17b37a59799280da12b8af
Kay <kard@hotmail.co.uk>
2022-06-14 15:46:22 +0100

    /add page shows a form to enter a name

--- app.rb
+++ app.rb
@@ -3,11 +3,19 @@ require 'sinatra/reloader'
 
 class MakersBNB < Sinatra::Base
   configure :development do
-    register Sinatra::reloader
+    register Sinatra::Reloader
   end
 
   get '/' do
     'Welcome to MakersBNB, the place to rent properties'
+  end
+
+  get '/makersbnb/add' do
+    erb(:'makersbnb/add')
+  end
+
+  post '/makersbnb/add' do
+    "Here is a confirmation of your new space details:"
   end
   
   run! if app_file == $0
---
+++ spec/features/add_space_spec.rb
@@ -0,0 +1,15 @@
+feature 'add a space' do
+  scenario 'it shows the label name field' do
+    visit '/makersbnb/add'
+    expect(page).to have_content 'Add your space!'
+    expect(page).to have_content 'Name'
+  end
+
+  scenario 'it adds a name of a space and shows confirmation' do
+    visit '/makersbnb/add'
+      fill_in 'Name', with: 'The Moon'
+      click_on 'Submit'
+      expect(page).to have_content 'Here is a confirmation of your new space details:'
+      expect(page).to have_content 'Name: The Moon'
+  end
+end
